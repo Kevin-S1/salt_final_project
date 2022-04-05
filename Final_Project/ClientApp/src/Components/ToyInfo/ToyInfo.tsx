9bd1dc1ba73d00d1ec8e9023f2a8669b3d65222a
BonnieMcb <bonniemcb@gmail.com>
2022-04-05 17:03:50 +0200

    Delete Toy component added and applied to ToyInfo

--- Final_Project/ClientApp/src/Components/ToyInfo/ToyInfo.tsx
+++ Final_Project/ClientApp/src/Components/ToyInfo/ToyInfo.tsx
@@ -1,49 +1,62 @@
-import React, {useCallback} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import { toyDetails } from '../../types';
 import {Link, useNavigate} from "react-router-dom";
 import './toyinfo.css';
 import { GoLocation } from 'react-icons/go';
 import { IoIosCheckmarkCircle } from 'react-icons/io';
+import DeleteModal from "../DeleteModal/DeleteModal";
 
 interface Props{
     toy: toyDetails,
     initialUserDetails: any
 }
 const ToyInfo = ({toy, initialUserDetails}: Props) => {
-    
+        const [click, setClick] = useState(false)
+        const [show, setShow] = useState(false)
+        
+        const showHandler = () => {
+            console.log('showhandler')
+            setShow(!show);
+        }
+        
         return (
-        <Link to={`/toys/${toy.id}`} >
-            <article className={toy.userId == initialUserDetails.id ? 'toy__card toy__card__owner' : 'toy__card'}>
-                <article className='row row__first'>
-                    <article className='col-7 toy__card__col__first'>
-                        <h3 className='toy__card__text toy__card__text--header'>{toy.name}</h3>
-                        <p className='toy__card__text toy__card__text--description'>{toy.description}</p>
-                        <article className='toy__card__info__row'>
-                            <h4 className='toy__card__text toy__card__text--location'>
-                            <GoLocation className='logo'/>{toy.userCity === undefined ? toy.user.city : toy.userCity} </h4>
-                            <h4 className={toy?.status === 0 ? 'toy__card__text--description toy--card--status toy--status__available' :
-                                (toy?.status === 1 ? 'toy__card__text--description toy--card--status toy--status__reserved' :
-                                    'toy__card__text--description toy--card--status toy--status__unavailable')}>
-                            <IoIosCheckmarkCircle  />{
-                                toy?.status === 0 ? 'Available' : (toy?.status === 1 ? 'Reserved' : 'Not Available')
-                            }</h4>
+            <article>
+                <Link to={`/toys/${toy.id}`} >
+                    <article className={toy.userId == initialUserDetails.id ? 'toy__card toy__card__owner' : 'toy__card'}>
+                        <article className='row row__first'>
+                            <article className='col-7 toy__card__col__first'>
+                                <h3 className='toy__card__text toy__card__text--header'>{toy.name}</h3>
+                                <p className='toy__card__text toy__card__text--description'>{toy.description}</p>
+                                <article className='toy__card__info__row'>
+                                    <h4 className='toy__card__text toy__card__text--location'>
+                                        <GoLocation className='logo'/>{toy.userCity === undefined ? toy.user.city : toy.userCity} </h4>
+                                    <h4 className={toy?.status === 0 ? 'toy__card__text--description toy--card--status toy--status__available' :
+                                        (toy?.status === 1 ? 'toy__card__text--description toy--card--status toy--status__reserved' :
+                                            'toy__card__text--description toy--card--status toy--status__unavailable')}>
+                                        <IoIosCheckmarkCircle  />{
+                                        toy?.status === 0 ? 'Available' : (toy?.status === 1 ? 'Reserved' : 'Not Available')
+                                    }</h4>
+                                </article>
+                            </article>
+                            <article className='col-5'>
+                                <img className='toy__card__image' src={toy.image}/>
+                            </article>
                         </article>
+                        
+
                     </article>
-                    <article className='col-5'>
-                        <img className='toy__card__image' src={toy.image}/>
-                    </article>
-                </article>
+                </Link>
                 {toy.userId == initialUserDetails.id.toString() ?
                     <article className='toy--owner--button--container'>
                         <Link className='toy--owner--button toy--owner--button__edit' to={`/edittoy/${toy.id}`}>Edit</Link>
-                        <button className='toy--owner--button toy--owner--button__delete'>Delete</button>
+                        <Link to={''}><button onClick={showHandler} className='toy--owner--button toy--owner--button__delete' >Delete</button> </Link>
+                        {show ? 
+                        <DeleteModal show={show} id={toy.id}/> : <></>}
                     </article> :
                     <></>
                 }
-                
-            </article>  
-
-        </Link>
+            </article>
+        
     )
 };
 
