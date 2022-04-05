56ad291def49d887492f6bfc2e07520c230bd90b
krishnamohanparuchuri <paruchurikris@gmail.com>
2022-04-05 17:07:37 +0200

    about and contact page

--- Final_Project/ClientApp/src/Components/About/AboutPage.tsx
+++ Final_Project/ClientApp/src/Components/About/AboutPage.tsx
@@ -1,24 +1,29 @@
 import React from "react";
-import logo1 from "../../HomePagePic1.svg";
+import './aboutpage.css';
 
 const AboutPage = () => {
     return(
         <div className="about-page__container" >
             <div className="about-page__top">
-                <h2>About Us</h2>   
+                <h4>Members of BorrowMy</h4>   
             </div>
             <div className="about-page__middle" >
-                <div className="about-page__header">Welcome to <strong className="nav-header__sub">Borrow</strong>My!</div>
-                <p className="about-page__paragraph"> The platform where you can borrow toys from your neighbours, and lend out toys that your kids play with only sometimes.
-                    Do your kids love new toys, but you don’t love the ever-growing mountain of vehicles, dolls and games spilling out of your cupboards?
-                    We have created a platform were you can connect with like-minded parents to lend toys to each other, creating a large toy library for your children.
-                </p>
-                <h4>Benefits of using BorrowMy:</h4>
-                <ul>
-                    <li>Reduce consumption, especially of plastics, which is better for the environment.</li>
-                    <li>Save money and have less clutter in your home.</li>
-                    <li>Kids get to play with new and exciting toys often.</li>
-                </ul>
+                <div className="about-page__profile">
+                    <img className="about-page__image" src="https://media.istockphoto.com/photos/side-view-longbeard-business-man-portrait-picture-id1074638206" />
+                    <p><strong>Kevin</strong>, CEO BorrowMY</p>
+                </div>
+                <div className="about-page__profile">
+                    <img className="about-page__image" src="https://media.istockphoto.com/photos/side-view-longbeard-business-man-portrait-picture-id1074638206" />
+                    <p><strong>Bonnie</strong>, Head Of Marketing</p>
+                </div>
+                <div className="about-page__profile">
+                    <img className="about-page__image" src="https://media.istockphoto.com/photos/side-view-longbeard-business-man-portrait-picture-id1074638206" />
+                    <p><strong>Samuel Lebender</strong>, Software Developer</p>
+                </div>
+                <div className="about-page__profile">
+                    <img className="about-page__image" src="https://media.istockphoto.com/photos/side-view-longbeard-business-man-portrait-picture-id1074638206" />
+                    <p><strong>Krishna Mohan Paruchuri</strong>, Finacial Advisor</p>
+                </div>
             </div>
             <div className="about-page__bottom" >
                 
